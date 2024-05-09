6781fbdd9e14dd40c01094b088da54f2d497d357
Oscar Riordan <o.w.riordan@gmail.com>
2024-05-09 18:32:58 +0100

    More posts

---
+++ src/routes/blog/[slug]/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let data;
+</script>
+
+<div class="layout">
+    <main>
+        <slot />
+    </main>
+
+    <aside>
+        <h2>More Posts</h2>
+        <ul>
+            {#each data.summaries as {slug, title}}
+                <li><a href={`/blog/${slug}`}>{title}</a></li>
+            {/each}
+        </ul>
+    </aside>
+</div>
+
+<style>
+    @media (min-width: 640px) {
+        .layout {
+            display: grid;
+            grid-template-columns: 1fr 16em;
+            gap: 2em;
+        }
+    }
+</style>
